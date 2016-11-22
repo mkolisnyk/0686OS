762695014ab0a57e5c5d5844800dee3bd62caa09
mykolak <mykola.kolisnyk@thetrainline.com>
2016-11-22 16:02:59 +0000

    ODT classes were created

---
+++ src/main/java/com/sample/framework/odt/ODTTestStep.java
@@ -0,0 +1,12 @@
+package com.sample.framework.odt;
+
+public class ODTTestStep extends ODTRunner {
+
+    public ODTTestStep() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void run() throws Exception {
+    }
+}
