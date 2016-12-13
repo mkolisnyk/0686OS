19db008d26b1fd97ce33bdfe18003fc3a50f65ba
mykolak <mykola.kolisnyk@thetrainline.com>
2016-12-13 01:04:27 +0000

    Final commit

--- src/test/java/com/sample/tests/junit/kdt/BankingKDTTest.java
+++ src/test/java/com/sample/tests/junit/kdt/BankingKDTTest.java
@@ -1,14 +1,6 @@
 package com.sample.tests.junit.kdt;
 
-import java.io.File;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.apache.commons.io.FileUtils;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-
-import com.sample.framework.utils.jira.JiraUtils;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -23,24 +15,7 @@ import cucumber.api.junit.Cucumber;
                 "usage:build/cucumber-usage.json"
                 },
         features = { "src/test/java/com/sample/tests/junit/kdt/features" },
-        glue = { "com/sample/tests/junit/kdt/steps" },
-        tags = {"@gen"}
+        glue = { "com/sample/tests/junit/kdt/steps" }
 )
 public class BankingKDTTest {
-	
-	@BeforeClass
-	public static void farBefore() throws Exception {
-		String featurePath = "build/features";
-		Map<String, String> content = JiraUtils.getField("http://localhost:9090", "mkolisnyk", "password",
-				"project = SAM AND type = Test AND status in (Done, Failed, Passed)", "description");
-		for (Entry<String, String> entry : content.entrySet()) {
-			System.out.println(entry.getKey());
-			System.out.println(entry.getValue());
-			String fileName = entry.getKey().replaceAll("(\\W+)", "_") + ".feature";
-			String result = "@gen @" + entry.getKey().split(" ")[0] + System.lineSeparator() + entry.getValue();
-			File output = new File(featurePath + File.separator + fileName);
-			output.getParentFile().mkdirs();
-			FileUtils.writeStringToFile(new File(featurePath + File.separator + fileName), result, "UTF-8");
-		}
-	}
 }
